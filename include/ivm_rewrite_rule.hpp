dd6d1d21e39aa72d294a3d7253e9ba36011eb055
kriti-sc <kathuriakriti1@gmail.com>
2023-07-11 15:22:58 +0200

    create replacement logicalget node

--- include/ivm_rewrite_rule.hpp
+++ include/ivm_rewrite_rule.hpp
@@ -7,6 +7,9 @@
 #include "duckdb/planner/planner.hpp"
 #include "duckdb/optimizer/optimizer.hpp"
 #include "duckdb/catalog/catalog_entry/view_catalog_entry.hpp"
+#include "duckdb/catalog/catalog_entry/table_catalog_entry.hpp"
+#include "duckdb/parser/tableref/basetableref.hpp"
+#include "duckdb/planner/tableref/bound_basetableref.hpp"
 
 namespace duckdb {
 
@@ -34,6 +37,7 @@ public:
 		}
 
 		printf("Activating the rewrite rule\n");
+		idx_t table_index = 2000;
 
 		// obtain view defintion from catalog
 		auto &catalog = Catalog::GetSystemCatalog(context);
@@ -52,67 +56,45 @@ public:
 		auto statement = parser.statements[0].get();
 		Planner planner(context);
 		planner.CreatePlan(statement->Copy());
-		printf("Plan: %s\n", planner.plan->ToString().c_str());
-
-		Parser newparser;
-		newparser.ParseQuery("SELECT * from delta_hello");
-		statement = newparser.statements[0].get();
-		Planner newplanner(context);
-		newplanner.CreatePlan(statement->Copy());
-		printf("Replace plan: %s\n", newplanner.plan->ToString().c_str());
-		auto new_node = dynamic_cast<LogicalGet*>(newplanner.plan->children[0].get());
-		for (int i=0;i<new_node->GetColumnBindings().size(); i++) {
-			printf("New node CB %d %s\n", i, new_node->GetColumnBindings()[i].ToString().c_str());
-		}
-
-		Parser multiexpparser;
-		multiexpparser.ParseQuery("SELECT _duckdb_ivm_multiplicity FROM delta_hello");
-		auto statement2 = multiexpparser.statements[0].get();
-		Planner multiexpplanner(context);
-		multiexpplanner.CreatePlan(statement2->Copy());
-		printf("multiplicity col plan: %s\n", multiexpplanner.plan->ToString().c_str());
-		auto mult_exp = std::move(multiexpplanner.plan->expressions[0]);
-
 		Optimizer optimizer((Binder&)planner.binder, context);
 		auto optimized_plan = optimizer.Optimize(std::move(planner.plan));
 		printf("Optimized plan: %s\n", optimized_plan->ToString().c_str());
 
 		auto modified_plan = std::move(optimized_plan);
-		modified_plan->expressions.emplace_back(std::move(mult_exp));
-//		modified_plan->types.emplace_back(LogicalType::BOOLEAN);
-//		modified_plan->MapTypes(modified_plan->types, {});
-//		modified_plan->MapBindings(modified_plan->GetColumnBindings(), {});
-
-		for (int i=0;i<modified_plan->GetColumnBindings().size(); i++) {
-			printf("Modified plan top node CB %d %s\n", i, modified_plan->GetColumnBindings()[i].ToString().c_str());
-		}
-
 		auto xchild = dynamic_cast<LogicalGet*>(modified_plan->children[0].get());
+		printf("Xchild node: %s\n", xchild->ToString().c_str());
 		auto col_bindings = xchild->GetColumnBindings();
 		for (int i=0;i<col_bindings.size(); i++) {
 			printf("CB %d %s\n", i, col_bindings[i].ToString().c_str());
 		}
-		modified_plan->children.clear();
+
+
 		printf("Create replacement node \n");
-		// the column names and return types of xchild and new_node will be the same
-		// TODO: cleanup this comment
-		auto replacement_node_return_types = new_node->returned_types;
-		// replacement_node_return_types.emplace_back(LogicalType::BOOLEAN);
-		auto replacement_node_names = new_node->names;
-		// replacement_node_names.emplace_back("_duckdb_ivm_multiplicity");
-		auto replacement_node = make_uniq<LogicalGet>(new_node->table_index, new_node->function,
-		                                              std::move(new_node->bind_data), replacement_node_return_types,
-		                                            	replacement_node_names);
-
-		for (int i=0;i<replacement_node_return_types.size();i++) {
-			printf("Replace node return type: %s, return names: %s\n", replacement_node_return_types[i].ToString().c_str(),
-			       replacement_node_names[i].c_str());
+		string delta_table = "delta_hello";
+		auto table_catalog_entry = Catalog::GetEntry(context, CatalogType::TABLE_ENTRY, "memory",
+		                                             "main",delta_table, OnEntryNotFound::THROW_EXCEPTION, error_context);
+		auto &table = table_catalog_entry->Cast<TableCatalogEntry>();
+		unique_ptr<FunctionData> bind_data;
+		auto scan_function = table.GetScanFunction(context, bind_data);
+		vector<LogicalType> return_types = {};
+		vector<string> return_names = {};
+		for (auto &col : table.GetColumns().Logical()) {
+			printf("creating bind data: %s\n", col.GetName().c_str());
+			return_types.push_back(col.Type());
+			return_names.push_back(col.Name());
 		}
+		auto replacement_get_node = make_uniq<LogicalGet>(table_index += 1, scan_function,
+		                                              std::move(bind_data), return_types,
+		                                              return_names);
 
-		replacement_node->column_ids = new_node->column_ids;
-		// replacement_node->column_ids.emplace_back()
-		replacement_node->projection_ids = new_node->projection_ids;
+
+
+
+
+
+
 		printf("Emplace back replacement node in modified plan \n");
+		modified_plan->children.clear();
 		modified_plan->children.emplace_back(std::move(replacement_node));
 		printf("Modified plan: %s %s\n", modified_plan->ToString().c_str(), modified_plan->ParamsToString().c_str());
 
@@ -127,7 +109,6 @@ public:
 		return;
 	}
 };
-
 }; // namespace duckdb
 
 #endif // DUCKDB_IVM_REWRITE_RULE_HPP
