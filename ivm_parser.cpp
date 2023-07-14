ba05e4731bd42b4315aca970536eb8c6a280f3fc
kriti-sc <kathuriakriti1@gmail.com>
2023-07-14 11:15:55 +0200

    modifying the grouping in aggregates

--- ivm_parser.cpp
+++ ivm_parser.cpp
@@ -6,6 +6,8 @@
 #include "duckdb/parser/query_node/select_node.hpp"
 #include "duckdb/parser/group_by_node.hpp"
 #include "duckdb/parser/tableref.hpp"
+#include "duckdb/planner/expression/bound_aggregate_expression.hpp"
+#include "duckdb/planner/operator/logical_aggregate.hpp"
 
 namespace duckdb {
 
@@ -56,58 +58,28 @@ ParserExtensionPlanResult IVMParserExtension::IVMPlanFunction(ParserExtensionInf
 
 	auto statement = dynamic_cast<SQLStatement*>(ivm_parse_data.statement.get());
 	printf("fetching statement from parse data: %s \n", statement->ToString().c_str());
-	if (statement->type == StatementType::SELECT_STATEMENT) {
-		auto select_statement = dynamic_cast<SelectStatement*>(statement);
-		auto select_node = dynamic_cast<SelectNode*>(select_statement->node.get());
-		printf("Select node: ");
-		for (int i = 0; i<select_node->select_list.size();i++) {
-			printf(", %s %d %d %d %s", select_node->select_list[i]->ToString().c_str(),
-			       	select_node->select_list[i]->IsAggregate(),
-			       select_node->select_list[i]->HasParameter(),
-			       select_node->select_list[i]->HasSubquery(),
-			       ExpressionClassToString(select_node->select_list[i]->GetExpressionClass()).c_str());
-		}
-
-
-		auto from_table_function = dynamic_cast<TableRef*>(select_node->from_table.get());
-		printf("\nFrom table func: %s \n", from_table_function->ToString().c_str());
-
-		auto &catalog = Catalog::GetSystemCatalog(context);
-
-	}
 
 	Planner planner(context);
 	planner.CreatePlan(statement->Copy());
 	printf("Trying to create plan by using plan cpp api: \n%s\n", planner.plan->ToString().c_str());
 
-	printf("Node 0: %s", LogicalOperatorToString(planner.plan->type).c_str());
-	auto t = planner.plan.get();
+	Optimizer optimizer((Binder&)planner.binder, context);
+	auto optimized_plan = optimizer.Optimize(std::move(planner.plan));
+	printf("Optimized plan: %s\n", optimized_plan->ToString().c_str());
 
-	printf("children: ");
-	for (int i = 0; i<planner.plan->children.size();i++) {
-		printf("%d: %s, ", i, planner.plan->children[i]->ToString().c_str());
-		printf("\n types: %s %s", LogicalOperatorToString(planner.plan->children[i]->type).c_str(),
-		       planner.plan->children[i]->ParamsToString().c_str());
-	}
-	printf("\n params to string: %s \n",planner.plan->ParamsToString().c_str());
+	printf("Level 2\n");
+	auto l2 = optimized_plan->children[0].get();
+	auto l2_aggop = dynamic_cast<LogicalAggregate*>(l2);
 
-	auto c = planner.plan->children[0]->children[0].get();
-	printf("Node 2: %s %s %s\n", c->ToString().c_str(), LogicalOperatorToString(c->type).c_str(),
-	       c->ParamsToString().c_str());
 
-	planner.plan->children[0]->children[0]->AddChild(planner.plan->Copy(context));
-	auto x = planner.plan->children[0]->children[0]->children[0].get();
-	printf("Node added: %s %s %s\n", x->ToString().c_str(), LogicalOperatorToString(x->type).c_str(),
-	       x->ParamsToString().c_str());
-
-	ParserExtensionPlanResult result;
-	result.function = IVMFunction();
-	result.parameters.push_back(Value::BIGINT(2));
-	// TODO: what is this? how to obtain this?
-	result.modified_databases = {};
-	result.requires_valid_transaction = false;
-	result.return_type = StatementReturnType::QUERY_RESULT;
-	return result;
+//	ParserExtensionPlanResult result;
+//	result.function = IVMFunction();
+//	result.parameters.push_back(Value::BIGINT(2));
+//	// TODO: what is this? how to obtain this?
+//	result.modified_databases = {};
+//	result.requires_valid_transaction = false;
+//	result.return_type = StatementReturnType::QUERY_RESULT;
+//	return result;
 	return ParserExtensionPlanResult();
 }
 
